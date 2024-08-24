0354aebdcd9fb68b662e14f2162ea16cb8997f30
Wenying Chang <changwenyingnl@gmail.com>
2024-08-24 15:57:22 +0200

    feat:/show name, type and size of upfile

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
+const upload = multer(); // middleware
 require('dotenv').config()
 
 var app = express();
@@ -14,6 +16,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  /*console.log(req.file);*/
+  const file = req.file;
+  const filename = file.originalname;
+  const filetype = file.mimetype;
+  const filesize = file.size;
+  
+  res.json({
+    name: filename,
+    type: filetype,
+    size: filesize,
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
